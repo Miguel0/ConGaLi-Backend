7757134ab04589b6f838a4db240f7d016654860b
Miguel Darío Isasmendi <miguelisasmendi@gmail.com>
2017-04-20 13:45:32 -0300

    Adding more tests

--- src/domain/model/User.js
+++ src/domain/model/User.js
@@ -1,3 +1,5 @@
+const AppException = require('../../exception/AppException')
+
 /**
  * This is a simple implementation of the user of the game. It lacks the security measure of encripting the password,
  * But it has the responsibility of knowing how to check it's own integrity and to create the JSON object that will
@@ -14,7 +16,37 @@ class User {
   }
 
   checkIntegrity () {
-    // TODO check integrity here
+    if (!this.createdOn || !(this.createdOn instanceof Date)) {
+      throw new AppException(
+        'error.user.integrity.createdOn.title',
+        'error.user.integrity.createdOn.body',
+        { user: this.toJSONObject() }
+      )
+    }
+
+    if (!this.name || !(typeof this.name === 'string' || this.name instanceof String)) {
+      throw new AppException(
+        'error.user.integrity.name.title',
+        'error.user.integrity.name.body',
+        { user: this.toJSONObject() }
+      )
+    }
+
+    if (!this.password || !(typeof this.password === 'string' || this.password instanceof String)) {
+      throw new AppException(
+        'error.user.integrity.authData.title',
+        'error.user.integrity.authData.body',
+        { user: this.toJSONObject() }
+      )
+    }
+
+    if (!this.color || !(typeof this.color === 'string' || this.color instanceof String) || !this.color.match(/^[a-f\d]{6}$/gi)) {
+      throw new AppException(
+        'error.user.integrity.color.title',
+        'error.user.integrity.color.body',
+        { user: this.toJSONObject() }
+      )
+    }
   }
 
   toJSONObject () {
@@ -23,6 +55,7 @@ class User {
     json.id = this.id
     json.name = this.name
     json.createdOn = this.createdOn
+    json.color = this.color
 
     return json
   }
--- test/unit/spec/domain/model/user.unit.js
+++ test/unit/spec/domain/model/user.unit.js
@@ -2,6 +2,7 @@ const chai = require('chai')
 const expect = chai.expect
 
 const User = require('../../../../../src/domain/model/User')
+const AppException = require('../../../../../src/exception/AppException')
 
 describe('User', function () {
   let user = null
@@ -12,7 +13,10 @@ describe('User', function () {
     expect(user).to.not.be.null
   })
 
-  user = new User()
+
+  beforeEach(() => {
+    user = new User()
+  })
 
   describe('Checking instance values upon creation', function () {
     it('should have default values set already', function () {
@@ -29,8 +33,9 @@ describe('User', function () {
       expect(jsonObject).to.have.property('id').and.to.be.null
       expect(jsonObject).to.have.property('name').and.to.be.a('string')
       expect(jsonObject).to.have.property('createdOn').and.not.be.null
+      expect(jsonObject).to.have.property('color').and.be.null
 
-      expect(Object.keys(jsonObject)).lengthOf(3)
+      expect(Object.keys(jsonObject)).lengthOf(4)
     })
   })
 
@@ -52,6 +57,65 @@ describe('User', function () {
       expect(jsonObject).to.have.property('name').and.to.be.a('string')
       expect(jsonObject).to.have.property('id').and.to.not.be.null
       expect(jsonObject).to.have.property('createdOn').and.to.not.be.null
+    })
+  })
+
+  describe('Checking integrity', function () {
+    it('missing or wrong type createdOn attribute', function () {
+      user.createdOn = null
+
+      expect(() => user.checkIntegrity())
+        .to.throw(AppException)
+        .and.to.have.property('titleKey')
+        .and.to.be.equal('error.user.integrity.createdOn.title')
+
+      user.createdOn = 'null'
+
+      expect(() => user.checkIntegrity())
+        .to.throw(AppException)
+        .and.to.have.property('titleKey')
+        .and.to.be.equal('error.user.integrity.createdOn.title')
+    })
+
+    it('missing or wrong type name attribute', function () {
+
+      expect(() => user.checkIntegrity())
+        .to.throw(AppException)
+        .and.to.have.property('titleKey')
+        .and.to.be.equal('error.user.integrity.name.title')
+
+      user.name = new Date()
+
+      expect(() => user.checkIntegrity())
+        .to.throw(AppException)
+        .and.to.have.property('titleKey')
+        .and.to.be.equal('error.user.integrity.name.title')
+    })
+
+    it('missing or wrong type password attribute', function () {
+      user.name= 'asdf'
+
+      expect(() => user.checkIntegrity())
+        .to.throw(AppException)
+        .and.to.have.property('titleKey')
+        .and.to.be.equal('error.user.integrity.authData.title')
+    })
+
+    it('missing or wrong type color attribute', function () {
+      user.name = 'asdf'
+      user.password = 'asdf'
+
+      expect(() => user.checkIntegrity())
+        .to.throw(AppException)
+        .and.to.have.property('titleKey')
+        .and.to.be.equal('error.user.integrity.color.title')
+
+      user.color = 'asdf'
+
+      expect(() => user.checkIntegrity())
+        .to.throw(AppException)
+        .and.to.have.property('titleKey')
+        .and.to.be.equal('error.user.integrity.color.title')
     })
   })
 })
